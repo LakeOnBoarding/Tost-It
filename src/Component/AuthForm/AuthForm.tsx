ecc3df7518df1e13b716a8820018e1c12615010d
ssujinchoi <wcsjinn@naver.com>
2023-06-05 16:23:47 +0900

    [feat] AuthForm회원가입input추가 및 비밀번호 보이기/숨기기 기능

--- src/Component/AuthForm/AuthForm.tsx
+++ src/Component/AuthForm/AuthForm.tsx
@@ -1,11 +1,16 @@
+import {useState} from 'react'
 import { Link } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { customAxios } from "../../API/customAxios";
 import { useNavigate,useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faEyeSlash } from '@fortawesome/free-regular-svg-icons';
 
 type MyFormData = {
   email: string;
   password: string;
+  passwordConfirm: string;
 };
 
 function AuthForm() {
@@ -16,8 +21,18 @@ function AuthForm() {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors, isValid },
   } = useForm<MyFormData>({ mode: "onChange" });
+
+  const password = watch("password");
+
+  const [inputType,setInputType] = useState('password')
+  const [hasInput, setHasInput] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setInputType(inputType === 'password' ? 'text' : 'password');
+  };
 
   const onSubmitHandler: SubmitHandler<MyFormData> = async (data) => {
     try {
@@ -28,7 +43,6 @@ function AuthForm() {
       console.error(err);
     }
   };
-
   return (
     <section className="flex flex-col justify-center items-center h-screen">
       <h1 className="mb-10 h-7 font-medium text-2xl">{location.pathname==='/signin' ? '로그인' : '이메일로 회원가입'}</h1>
@@ -54,29 +68,66 @@ function AuthForm() {
             <span className="text-red text-xsm ">{errors.email.message}</span>
           )}
         </fieldset>
-        <fieldset className="block mb-4">
+        <fieldset className="block mb-4 relative">
           <label className="text-xs" htmlFor="password">
             비밀번호
           </label>
           <input
             className="w-80 text-sm px-2 py-2 border-b block outline-0 focus:border-sky-500"
-            type="password"
+            type={inputType}
             id="password"
             placeholder="특수문자,숫자,영문자 조합 8글자 이상"
             {...register("password", {
               required: "비밀번호를 입력해주세요",
               pattern: {
                 value:
-                  /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[*!@#$%^&+=()-_])(?=.*[a-zA-Z\d@#$%^&+=]).{8,}$/,
+                  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/,
                 message:
                   "특수문자, 숫자, 영문자 조합 8글자 이상을 입력해주세요.",
               },
             })}
+            onChange={e => {
+              setHasInput(e.target.value !== '');
+              register("password").onChange(e); // 통합된 onChange 이벤트 핸들러 실행
+            }}
           />
+{
+  hasInput && (
+    inputType === 'password' ? (
+      <FontAwesomeIcon icon={ faEyeSlash } onClick={togglePasswordVisibility} className="cursor-pointer absolute right-2 top-9"/>
+      ) : (
+        <FontAwesomeIcon icon={ faEye } onClick={togglePasswordVisibility} className="cursor-pointer absolute right-2 top-9"/>
+      ) 
+  )
+}
           {errors.password && (
             <span className="text-red text-xsm">{errors.password.message}</span>
           )}
-        </fieldset>
+          </fieldset>
+          {location.pathname === '/signup' ? 
+            (
+              <fieldset>
+                <label className="text-xs" htmlFor="passwordConfirm">
+                비밀번호 확인
+                </label>
+                <input
+                  className="w-80 text-sm px-2 py-2 border-b block outline-0 focus:border-sky-500"
+                  type="password"
+                  id="passwordConfirm"
+                  placeholder="비밀번호 재입력"
+                  {...register("passwordConfirm", {
+                    required: "비밀번호를 한번 더 입력해주세요",
+                    validate: value =>
+                      value === password || "입력한 비밀번호가 일치하지 않습니다."
+                  })}
+                />
+                {errors.passwordConfirm && (
+                <span className="text-red text-xsm">{errors.passwordConfirm.message}</span>
+                )}
+              </fieldset>
+            ) : (null)
+          }
+        
         <button
           type="submit"
           className={`text-white font-bold py-2 h-11 mt-8 mb-5 rounded-full text-sm ${
