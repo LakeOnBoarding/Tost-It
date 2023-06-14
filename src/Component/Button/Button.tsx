2d53219b16041aafabd8f11576b1534d1f13aeae
EllaSEON <shineunhye5@gmail.com>
2023-06-14 11:35:23 +0900

    [feat] Button 컴포넌트 마크업

---
+++ src/Component/Button/Button.tsx
@@ -0,0 +1,26 @@
+interface ButtonProps {
+  onClick: () => void;
+  children: string;
+  size?: "small" | "medium" | "large";
+}
+
+function Button({ onClick, children, size }: ButtonProps) {
+  let buttonClassName =
+    "bg-point_blue text-white shadow shadow-black font-semibold";
+
+  if (size === "small") {
+    buttonClassName += " rounded-3xl w-12 px-1.5 py-3 mx-2 "; // 작은 사이즈 스타일
+  } else if (size === "medium") {
+    buttonClassName += " rounded-full w-44 px-1.5 py-3 mx-2 "; // 큰 사이즈 스타일
+  } else if (size === "large") {
+    buttonClassName += " rounded-full w-96 px-1.5 py-3"; // 기본 사이즈 스타일
+  }
+
+  return (
+    <button className={buttonClassName} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
--- src/Pages/Todo/Todo.tsx
+++ src/Pages/Todo/Todo.tsx
@@ -1,5 +1,6 @@
 // import { useContext } from "react";
 // import { UserContext } from "../context/UserContext";
+import Button from "../../Component/Button/Button";
 
 function Todo() {
   // const token = useContext(UserContext);
@@ -20,19 +21,19 @@ function Todo() {
           <div className="w-50 h-32 bg-post_red">포스트잇1개</div>
         </div>
       </section>
-      <section className="w-96  m-5 flex-row text-lx ">
-        <button className="w-12 px-1.5 py-3 mx-2 rounded-3xl bg-point_blue text-white">
+      <section className="w-96  mt-5 flex-row text-lx ">
+        <Button size="small" onClick={() => console.log("아침")}>
           아침
-        </button>
-        <button className="w-12 px-1.5 py-3 mx-2 rounded-3xl bg-point_blue text-white">
+        </Button>
+        <Button size="small" onClick={() => console.log("점심")}>
           점심
-        </button>
-        <button className="w-12 px-1.5 py-3 mx-2 rounded-3xl bg-point_blue text-white ">
+        </Button>
+        <Button size="small" onClick={() => console.log("저녁")}>
           저녁
-        </button>
-        <button className="w-32 px-1.5 py-3 mx-2 rounded-3xl bg-point_blue text-white">
-          오늘의 할일
-        </button>
+        </Button>
+        <Button size="medium" onClick={() => console.log("gkfdlf")}>
+          오늘의 할 일
+        </Button>
       </section>
     </div>
   );
